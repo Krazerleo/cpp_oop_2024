30743ecb78e6e2b0488d2fb9b4e903bbb7fc468f
krazerleo <krazerleo@gmail.com>
2024-03-28 00:50:21 +0700

    init

---
+++ headers/node.h
@@ -0,0 +1,22 @@
+#pragma once
+
+template <typename T> class BaseNode {
+public:
+  BaseNode *next;
+  BaseNode *prev;
+
+public:
+  BaseNode() : _data() {}
+  explicit BaseNode(const T &data) : _data(data) {}
+  explicit BaseNode(T &&data) : _data(std::move(data)) {}
+
+public:
+  T &getData() { return _data; }
+
+  void setData(const T &data) { _data = data; }
+
+  virtual void setData(T &&data) { _data = std::move(data); }
+
+private:
+  T _data;
+};
